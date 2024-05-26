7b69a90219e19567464c5f5ed284de3828f9e617
brunolucy2023x <lucyb5147@gmail.com>
2024-05-26 17:07:11 +0000

    forty

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,35 +1,35 @@
 #!/usr/bin/python3
-# brunookoth44@gmail.com 
-# 2024
+"""Flask server (variable app)
 """
-The RESTful api starts here. The api aids data access in the app.
-"""
-from os import getenv
+
 
 from flask import Flask, jsonify
-from flask_cors import CORS
-
-from api.v1.views import app_views
 from models import storage
+from os import getenv
+from api.v1.views import app_views
 
 app = Flask(__name__)
-app.url_map.strict_slashes = False
 app.register_blueprint(app_views)
-CORS(app, origins=["0.0.0.0"])
-host = getenv("HBNB_API_HOST", "0.0.0.0")
-port = getenv("HBNB_API_PORT", "5000")
+app.url_map.strict_slashes = False
 
 
 @app.teardown_appcontext
-def teardown(exception):
-    """Cleanup operations"""
+def downtear(self):
+    '''Status of your API'''
     storage.close()
 
 
 @app.errorhandler(404)
-def not_found(error):
-    return jsonify({"error": "Not found"}), 404
+def page_not_found(error):
+    '''return render_template'''
+    return jsonify('error='Not found'), 404
 
 
 if __name__ == "__main__":
-    app.run(host, port, threaded=True, debug=True)
+    host = getenv('HBNB_API_HOST')
+    port = getenv('HBNB_API_PORT')
+    if not host:
+        host = '0.0.0.0'
+    if not port:
+        port = '5000'
+    app.run(host=host, port=port, threaded=True)
